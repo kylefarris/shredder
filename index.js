1df1ae0c07aca72678185108248b57e01f8639eb
Kyle Farris <kylefarris@gmail.com>
2020-10-06 11:51:04 -0400

    Added git pre-commit/push hooks with Husky. Added new config files for linting/formatting code.

--- index.js
+++ index.js
@@ -17,12 +17,10 @@ function buildShredFlags(settings) {
     if (settings.force === true) flags.push('-f');
 
     // How many overwrite iterations?
-    if (typeof settings.iterations == 'number')
-        flags.push(`--iterations=${settings.iterations}`);
+    if (typeof settings.iterations === 'number') flags.push(`--iterations=${settings.iterations}`);
 
     // How many bytes to shred
-    if (settings.bytes && settings.bytes.match(/^\d+[KMG]?$/))
-        flags.push(`--size=${settings.bytes}`);
+    if (settings.bytes && settings.bytes.toString().match(/^\d+[KMG]?$/)) flags.push(`--size=${settings.bytes}`);
 
     // Should we actually remove the file?
     if (settings.remove) flags.push('-u');
@@ -32,27 +30,25 @@ function buildShredFlags(settings) {
 
     // Build the String
     return flags;
-
 }
-
 class ShredFile {
     /**
-    * This callback is displayed as part of the ShredFile class.
-    *
-    * @callback ShredFile~endCallback
-    * @param {Error} err - A standard error message (null if no error)
-    * @param {float} file - The original files parameter passed into this shred method.
-    */
+     * This callback is displayed as part of the ShredFile class.
+     *
+     * @callback ShredFile~endCallback
+     * @param {Error} err - A standard error message (null if no error)
+     * @param {string | Array} file - The original files parameter passed into this shred method.
+     */
 
     /**
-    * This callback is displayed as part of the ShredFile class.
-    *
-    * @callback ShredFile~statusCallback
-    * @param {string} action - This will be either 'overwriting' or 'renaming'
-    * @param {float} progress - The percentage of the specific action that is complete (ex. 0.66)
-    * @param {string} file - File name of the file that is currently being acted upon
-    * @param {string} activeFilePath - Full path to the file that is currently being acted upon
-    */
+     * This callback is displayed as part of the ShredFile class.
+     *
+     * @callback ShredFile~statusCallback
+     * @param {string} action - This will be either 'overwriting' or 'renaming'
+     * @param {number} progress - The percentage of the specific action that is complete (ex. 0.66)
+     * @param {string | Array} file - File name of the file that is currently being acted upon
+     * @param {string} activeFilePath - Full path to the file that is currently being acted upon
+     */
 
     /**
      * ShredFile Class constructor.
@@ -66,10 +62,10 @@ class ShredFile {
      * @param {boolean} [settings.zero=true] - If true, add a final overwrite with zeros to hide shredding
      * @param {boolean} [settings.debugMode=fakse] - If true, turns on debugging (logs to console)
      * @example
-     * 
+     *
      * // Instantiat with all defaults
      * const shred = new ShredFile();
-     * 
+     *
      * // Instantiate with some settings overrides
      * const shredder = new ShredFile({ iteractions: 5, debugMode: true });
      */
@@ -87,14 +83,13 @@ class ShredFile {
         this.settings = { ...defaults, ...settings };
 
         // Verify specified paths exists
-        this.shredPath_exists = false;
+        this.shredPathExists = false;
 
         // REQUIRED: Make sure shred exists at specified location
         if (existsSync(this.settings.shredPath)) {
-            this.shredPath_exists = true;
-        } else {
-            if (this.settings.debugMode)
-                console.log(`shredfile: shred could not be found at ${this.shredPath}!`);
+            this.shredPathExists = true;
+        } else if (this.settings.debugMode) {
+            console.error(`shredfile: shred could not be found at ${this.shredPath}!`);
         }
 
         // Build shred flags
@@ -111,23 +106,23 @@ class ShredFile {
      * @param {ShredFile~endCallback} [endCb] - (optional) This will be called when done.
      * @returns {Promise<object>} If no `endCb` is provided, a Promise will be returned.
      * @example
-     * 
+     *
      * const shredder = new ShredFile();
-     * 
+     *
      * // Do some shredding (Promise-style)
      * async function doSomeShredding() {
      *     // Shred a single file
      *     const result = await shredder.shred('/path/to/some/file');
-     * 
+     *
      *     // Shred multiple files
      *     const result = await shredder.shred(['/path/to/first/file', '/path/to/second/file']);
-     * 
+     *
      *     // Shred a file with notifications on progress
      *     const result = await shredder.shred('/another/file/to/shred', (action, progress, file, activeFilePath) => {
      *         console.log(`Currently ${action} ${activeFilePath} (${(progress * 100).floor()}%));
      *     });
      * }
-     * 
+     *
      * // Do some shredding (callback-style)
      * function moreShredding(cb) {
      *     shredder.shred('/path/to/file/file', null, (err, file) => {
@@ -145,7 +140,7 @@ class ShredFile {
     async shred(files, statusCb, endCb) {
         const self = this;
         let hasCb = false;
-        
+
         // Verify second param, if supplied, is a function
         if (endCb && typeof endCb !== 'function') {
             throw new Error('Invalid endCb provided to init method. Third paramter, if provided, must be a function!');
@@ -153,9 +148,9 @@ class ShredFile {
             hasCb = true;
         }
 
-        // eslint-disable-next-line no-async-promise-executor
+        // eslint-disable-next-line no-async-promise-executor, consistent-return
         return new Promise(async (resolve, reject) => {
-            const origFiles = files;
+            // const origFiles = files;
             if (typeof files === 'string') files = [files];
 
             if (!Array.isArray(files) || files.length <= 0) {
@@ -163,7 +158,7 @@ class ShredFile {
                     console.log('shredfile: No file(s) specified to shred!', typeof files, files);
                 }
                 const err = new Error('No file(s) specified to shred!');
-                return (hasCb ? endCb(err, null) : reject(err));
+                return hasCb ? endCb(err, null) : reject(err);
             }
 
             // For storing file name being actively shredded
@@ -177,7 +172,6 @@ class ShredFile {
             const shred = spawn(this.settings.shredPath, options);
             if (this.settings.debugMode === true)
                 console.log(`shredfile: Configured shred command: ${this.settings.shredPath} ${options.join(' ')}`);
-
 
             // What to do if there's an error...
             shred.stderr.on('data', (data) => {
@@ -210,8 +204,8 @@ class ShredFile {
                         if (matches !== -1) {
                             activeFilePath = path.dirname(matches[1]);
                             file = path.basename(matches[1]);
-                            const numerator = parseInt(matches[2]);
-                            const denominator = parseInt(matches[3]);
+                            const numerator = parseInt(matches[2], 10);
+                            const denominator = parseInt(matches[3], 10);
                             if (denominator !== 0) progress = numerator / denominator;
                             if (statusCb && typeof statusCb === 'function')
                                 return statusCb('overwriting', progress, file, activeFilePath);
@@ -219,13 +213,14 @@ class ShredFile {
                         matches = data.match(/renamed to (\/.*)$/);
                         if (Array.isArray(matches)) {
                             rename = path.basename(matches[1]).trim();
-                            if (!/^[0]+$/.test(rename)) return;
+                            if (!/^[0]+$/.test(rename)) return null;
                             if (file.length > 0) progress = rename.length / file.length;
                             if (statusCb && typeof statusCb === 'function')
                                 return statusCb('renaming', progress, file, activeFilePath);
                         }
                     }
                 }
+                return null;
             });
         });
     }
